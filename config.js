80e57e05b6966eb4645caaff56479b94c7c69b59
khattab88 <m_khatab_88@hotmail.com>
2020-10-11 02:00:48 +0200

    finalize email service

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 require('custom-env').env(true);
 
 module.exports = {
--- test.js
+++ test.js
@@ -2,13 +2,13 @@ const config = require("./config");
 const EmailService = require("./services/email-service");
 
 const mailOptions = {
-    from: "Expresso App <expressoapp.2020@gmail.com>",
+    from: `Expresso App <${config.email_address}>`,
     email: config.email_address,
     subject: "test email",
-    message: `test email from email service , ${process.env.NODE_ENV}`,
+    message: `Test email from email service \n Environment: ${process.env.NODE_ENV}`,
     //html: options.html
 };
 
 const emailSvc = new EmailService(config.email_host, config.email_address, config.email_password);
-emailSvc.sendEmailProd(mailOptions);
+emailSvc.sendEmail(mailOptions);
 
