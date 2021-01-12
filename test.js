fe29a1887b0f25db304a86cadd0087c0fb540a6c
khattab88 <m_khatab_88@hotmail.com>
2021-01-12 14:18:01 +0200

    send emails using sendgrid

--- test.js
+++ test.js
@@ -2,14 +2,14 @@ const config = require("./config");
 const { EmailService } = require("./index");
 
 const options = {
-    from: `Expresso App <${config.email_address}>`,
-    to: "expressoapp.2020@gmail.com",
+    from: `Expresso App <${config.email.address}>`,
+    to: "m.khatab.88@gmail.com",
     subject: "test email",
-    text: `Test email \n Environment: ${process.env.NODE_ENV}`,
-    html: "<p>hello form test email</p>"
+    html: "<strong style='font-style: italic;'>hello form test email</strong>",
+    text: `this is a test email \n Environment: ${process.env.NODE_ENV}`,
 };
 
-const emailSvc = new EmailService(config.email_host, config.email_address, config.email_password);
-emailSvc.sendEmail(options);
+// console.log(config.email);
 
-console.log("FINISH!");
+const emailSvc = new EmailService(config.email.host, config.email.userName, config.email.password);
+emailSvc.sendEmail(options);
